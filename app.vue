585bb3c4b5416a4c4de6442a409ce98699e1ca46
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2023-04-12 15:53:47 +0200

    feat: set up tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    <base-divider />
+  <div :style="{ backgroundImage: 'url(/images/backgrounds/grid.svg)' }" class="layout-default z-50 select-none bg-repeat leading-relaxed tracking-tight text-gray-900 antialiased">
+    <main class="container relative mt-6 sm:mt-8">
+      <div class="h-20"></div>
+    </main>
   </div>
 </template>
+
+<!-- -------------------------------------------------------------------------
+     STYLE
+     ------------------------------------------------------------------------- -->
+
+<style lang="scss">
+html {
+  @apply bg-gray-100 #{!important};
+}
+
+* {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-tap-highlight-color: transparent;
+}
+
+a,
+img {
+  -webkit-user-drag: none;
+}
+
+a {
+  @apply outline-none #{!important};
+
+  &:focus {
+    &:not(.no-link-focus-style) {
+      @apply text-black #{!important};
+    }
+  }
+}
+
+img {
+  user-select: none;
+}
+</style>
