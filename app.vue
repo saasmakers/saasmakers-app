37b8e72d15e6478118cbfc6de94bfc07850ab888
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2023-05-08 19:05:29 +0200

    feat: set node version and tailwind

--- app.vue
+++ app.vue
@@ -3,7 +3,10 @@
      ------------------------------------------------------------------------- -->
 
 <template>
-  <div :style="{ backgroundImage: 'url(/images/backgrounds/grid.svg)' }" class="layout-default z-50 select-none bg-repeat leading-relaxed tracking-tight text-gray-900 antialiased">
+  <div
+    :style="{ backgroundImage: 'url(/images/backgrounds/grid.svg)' }"
+    class="layout-default bg-gray-100 absolute inset-0 z-50 select-none bg-repeat leading-relaxed tracking-tight text-gray-900 antialiased"
+  >
     <the-header />
 
     <main class="container relative mt-6 sm:mt-8">
@@ -11,37 +14,3 @@
     </main>
   </div>
 </template>
-
-<!-- -------------------------------------------------------------------------
-     STYLE
-     ------------------------------------------------------------------------- -->
-
-<style lang="scss">
-html {
-  @apply bg-gray-100 #{!important};
-}
-
-* {
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  -webkit-tap-highlight-color: transparent;
-}
-
-a,
-img {
-  -webkit-user-drag: none;
-}
-
-a {
-  @apply outline-none #{!important};
-
-  &:focus {
-    &:not(.no-link-focus-style) {
-      @apply text-black #{!important};
-    }
-  }
-}
-
-img {
-  user-select: none;
-}
-</style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ module.exports = {
 
   mode: "jit",
 
-  purge: [`components/**/*.{vue,js}`, `layouts/**/*.vue`, `pages/**/*.vue`, `plugins/**/*.{js,ts}`, `nuxt.config.{js,ts}`],
+  purge: [`app.vue`, `components/**/*.{vue,js}`, `layouts/**/*.vue`, `pages/**/*.vue`, `plugins/**/*.{js,ts}`, `nuxt.config.{js,ts}`],
 
   theme: {
     borderColor: (theme) => ({
