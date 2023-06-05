2a03193fa121b9d386c2e899ef51fad793b6c918
Valerian Saliou <valerian@valeriansaliou.name>
2023-06-05 15:46:06 +0200

    feat: add base-space component

---
+++ src/components/base/BaseSpace.vue
@@ -0,0 +1,47 @@
+<!--
+ * SaaS Makers - Web
+ *
+ * Copyright 2023, SaaS Makers
+ * Author: Valerian Saliou https://valeriansaliou.name/
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+span.c-base-space(
+  v-html="spacesHtml"
+)
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script>
+export default {
+  name: "BaseSpace",
+
+  props: {
+    repeat: {
+      type: Number,
+      default: 1
+    }
+  },
+
+  computed: {
+    spacesHtml() {
+      return Array(this.repeat).fill("&nbsp;").join("");
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-space";
+</style>
