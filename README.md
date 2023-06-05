e24c55b2f30df5d430618f1610b7e69e4942986f
Valerian Saliou <valerian@valeriansaliou.name>
2023-06-05 09:48:49 +0200

    chore: bootstrap basis

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # saasmakers-web
 
 SaaS Makers website.
+
+**Copyright 2023, SaaS Makers**
+
+## Build Setup
+
+Built with [Nuxt](https://nuxtjs.org/). See the commands below for reference on how to build the website, or develop.
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build static assets for production
+$ npm run build
+
+# run all tests
+$ npm test
+
+# run lints
+$ npm run lint
+
+# clean all static assets previously built
+$ npm run clean
+```
---
+++ src/layouts/default.vue
@@ -0,0 +1,31 @@
+<!--
+ * SaaS Makers - Web
+ *
+ * Copyright 2023, SaaS Makers
+ * Author: Valerian Saliou https://valeriansaliou.name/
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+page-container
+  page-header
+
+  page-content
+    nuxt
+
+  page-entice
+  page-footer
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script>
+export default {
+  name: "DefaultLayout"
+};
+</script>
