e2e64da41cae720dc87537177ed277e4ac13c807
Valerian Saliou <valerian@valeriansaliou.name>
2023-06-06 09:31:33 +0200

    feat: floating page-header

--- src/components/page/PageHeader.vue
+++ src/components/page/PageHeader.vue
@@ -10,35 +10,46 @@
      ********************************************************************** -->
 
 <template lang="pug">
-.c-page-header
-  page-wrapper
-    .c-page-header__inner
-      .c-page-header__left
-        nuxt-link(
-          to="/"
-          class="c-page-header__logo"
-        )
-          base-logo
+div(
+  :class=`[
+    "c-page-header",
+    {
+      "c-page-header--floating": isFloating
+    }
+  ]`
+)
+  .c-page-header__sticky
+    .c-page-header__bar
+      page-wrapper
+        .c-page-header__inner
+          .c-page-header__left
+            nuxt-link(
+              to="/"
+              class="c-page-header__logo"
+            )
+              base-logo
 
-      .c-page-header__right
-        a(
-          :href="$config.platforms.youtube.target"
-          target="_blank"
-          class="c-page-header__platform"
-        )
-          base-button(
-            button-class="c-page-header__platform-button"
-            icon="youtube"
-            tint="red"
-            borderless
-            bolder
-            round
-          )
-            span.c-page-header__platform-handle.c-page-header__platform-handle--large
-              | {{ $config.platforms.youtube.handle }}
+          .c-page-header__right
+            a(
+              :href="$config.platforms.youtube.target"
+              target="_blank"
+              class="c-page-header__platform"
+            )
+              base-button(
+                button-class="c-page-header__platform-button"
+                icon="youtube"
+                tint="red"
+                borderless
+                bolder
+                round
+              )
+                span.c-page-header__platform-handle.c-page-header__platform-handle--large
+                  | {{ $config.platforms.youtube.handle }}
 
-            span.c-page-header__platform-handle.c-page-header__platform-handle--small
-              | YT
+                span.c-page-header__platform-handle.c-page-header__platform-handle--small
+                  | YT
+
+  .c-page-header__ghost
 </template>
 
 <!-- **********************************************************************
@@ -46,8 +57,53 @@
      ********************************************************************** -->
 
 <script>
+// CONSTANTS
+const SCROLLED_THRESHOLD_VERTICAL = 30;
+
 export default {
-  name: "PageHeader"
+  name: "PageHeader",
+
+  data() {
+    return {
+      // --> STATE <--
+
+      isFloating: false
+    };
+  },
+
+  beforeMount() {
+    // Bind scroll listener
+    window.addEventListener("scroll", this.onWindowScroll);
+  },
+
+  mounted() {
+    // Process first scroll event (eg. the page is reloaded on a scrolled \
+    //   page, therefore we need to emulate a scroll event to compute first \
+    //   state)
+    this.onWindowScroll();
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onWindowScroll);
+  },
+
+  methods: {
+    // --> EVENT LISTENERS <--
+
+    /**
+     * Triggers when window scrolls
+     * @public
+     * @return {undefined}
+     */
+    onWindowScroll() {
+      const _isFloating = window.scrollY >= SCROLLED_THRESHOLD_VERTICAL;
+
+      // Update reactive marker? (only if changed)
+      if (_isFloating !== this.isFloating) {
+        this.isFloating = _isFloating;
+      }
+    }
+  }
 };
 </script>
 
@@ -59,8 +115,39 @@ export default {
 $c: ".c-page-header";
 
 .c-page-header {
+  padding-bottom: 12px;
+
+  #{$c}__sticky,
+  #{$c}__ghost,
+  #{$c}__bar {
+    height: $page-header-height-normal;
+  }
+
+  #{$c}__sticky,
+  #{$c}__ghost {
+    border-bottom: 1px solid transparent;
+  }
+
+  #{$c}__sticky,
+  #{$c}__bar {
+    transition: all 250ms linear;
+    transition-property: background-color, border-color;
+  }
+
+  #{$c}__sticky {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+  }
+
+  #{$c}__bar {
+    display: flex;
+    align-items: center;
+  }
+
   #{$c}__inner {
-    padding: 24px 0 36px;
     display: flex;
     align-items: center;
   }
@@ -98,14 +185,29 @@ $c: ".c-page-header";
     padding-left: 12px;
     flex: 0 0 auto;
   }
+
+  // --> BOOLEANS <--
+
+  &--floating {
+    #{$c}__sticky {
+      border-bottom-color: rgba($color-base-black-light, 0.1);
+    }
+
+    #{$c}__bar {
+      background-color: rgba($color-background-white, 0.75);
+      backdrop-filter: blur(6px) saturate(160%) contrast(45%) brightness(140%);
+    }
+  }
 }
 
 // --> MEDIA-QUERIES <--
 
 @media (max-width: $screen-lilliput-width-breakpoint) {
   .c-page-header {
-    #{$c}__inner {
-      padding: 18px 0 28px;
+    #{$c}__sticky,
+    #{$c}__ghost,
+    #{$c}__bar {
+      height: $page-header-height-small;
     }
 
     #{$c}__platform {
